e05e426f354dac4b6acb189039bd6b3b9fa862d2
yellow chicks <cdh_cjx@163.com>
2017-06-07 21:20:09 +0800

    Update jsoniter_customize_time.go

--- jsoniter_customize_time.go
+++ jsoniter_customize_time.go
@@ -10,26 +10,6 @@ func TimeDecoder(ptr unsafe.Pointer, iter *Iterator) {
 	*((*time.Time)(ptr)) = *t
 }
 
-// 需要实现全部标准
-const (
-	RFC3339     = "2006-01-02T15:04:05Z07:00"
-	ANSIC       = "Mon Jan _2 15:04:05 2006"
-	UnixDate    = "Mon Jan _2 15:04:05 MST 2006"
-	RubyDate    = "Mon Jan 02 15:04:05 -0700 2006"
-	RFC822      = "02 Jan 06 15:04 MST"
-	RFC822Z     = "02 Jan 06 15:04 -0700" // RFC822 with numeric zone
-	RFC850      = "Monday, 02-Jan-06 15:04:05 MST"
-	RFC1123     = "Mon, 02 Jan 2006 15:04:05 MST"
-	RFC1123Z    = "Mon, 02 Jan 2006 15:04:05 -0700" // RFC1123 with numeric zone
-	RFC3339Nano = "2006-01-02T15:04:05.999999999Z07:00"
-	Kitchen     = "3:04PM"
-	// Handy time stamps.
-	Stamp      = "Jan _2 15:04:05"
-	StampMilli = "Jan _2 15:04:05.000"
-	StampMicro = "Jan _2 15:04:05.000000"
-	StampNano  = "Jan _2 15:04:05.000000000"
-)
-
 func ReadTime(iter *Iterator) *time.Time {
 	var (
 		bts   []byte
