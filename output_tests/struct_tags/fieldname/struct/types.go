ddfbb0c62ead39e32bcaebd3b36ddf8a567ae189
Tim Hockin <thockin@google.com>
2017-06-30 08:16:43 -0700

    better tests for field name tags

---
+++ output_tests/struct_tags/fieldname/struct/types.go
@@ -0,0 +1,29 @@
+package test
+
+type S1 struct {
+	S1F string
+}
+type S2 struct {
+	S2F string
+}
+type S3 struct {
+	S3F string
+}
+type S4 struct {
+	S4F string
+}
+type S5 struct {
+	S5F string
+}
+type S6 struct {
+	S6F string
+}
+
+type T struct {
+	F1 S1 `json:"F1"`
+	F2 S2 `json:"f2"`
+	F3 S3 `json:"-"`
+	F4 S4 `json:"-,"`
+	F5 S5 `json:","`
+	F6 S6 `json:""`
+}
