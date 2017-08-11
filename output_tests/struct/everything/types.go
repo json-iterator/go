7df5a67d0d6760163a9491446f9c14a08ca34054
Tim Hockin <thockin@google.com>
2017-08-10 20:58:49 -0700

    Add tests for int64

--- output_tests/struct/everything/types.go
+++ output_tests/struct/everything/types.go
@@ -8,9 +8,11 @@ type typeForTest struct {
 	Int8    int8
 	Int16   int16
 	Int32   int32
+	Int64   int64
 	Uint8   uint8
 	Uint16  uint16
 	Uint32  uint32
+	Uint64  uint64
 	Float32 float32
 	Float64 float64
 	String1 string
