002b5ae34214fdc0bad508c2ed6f572fa96dfdda
Tao Wen <taowen@gmail.com>
2018-02-05 23:45:42 +0800

    fix tests

--- api_tests/decoder_test.go
+++ api_tests/decoder_test.go
@@ -2,16 +2,16 @@ package test
 
 import (
 	"bytes"
-	"encoding/json"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"github.com/json-iterator/go"
 )
 
 func Test_disallowUnknownFields(t *testing.T) {
 	should := require.New(t)
 	type TestObject struct{}
 	var obj TestObject
-	decoder := json.NewDecoder(bytes.NewBufferString(`{"field1":100}`))
+	decoder := jsoniter.NewDecoder(bytes.NewBufferString(`{"field1":100}`))
 	decoder.DisallowUnknownFields()
 	should.Error(decoder.Decode(&obj))
 }
