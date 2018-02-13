eb9aeccee2a2c6d09203508100cc26c829852d3e
Tao Wen <taowen@gmail.com>
2018-02-13 15:48:39 +0800

    consolidate builtin tests

---
+++ type_tests/builtin_test.go
@@ -0,0 +1,46 @@
+package test
+
+func init() {
+	testCases = append(testCases,
+		(*bool)(nil),
+		(*boolAlias)(nil),
+		(*byte)(nil),
+		(*byteAlias)(nil),
+		(*float32)(nil),
+		(*float32Alias)(nil),
+		(*float64)(nil),
+		(*float64Alias)(nil),
+		(*int8)(nil),
+		(*int8Alias)(nil),
+		(*int16)(nil),
+		(*int16Alias)(nil),
+		(*int32)(nil),
+		(*int32Alias)(nil),
+		(*int64)(nil),
+		(*int64Alias)(nil),
+		(*string)(nil),
+		(*stringAlias)(nil),
+		(*uint8)(nil),
+		(*uint8Alias)(nil),
+		(*uint16)(nil),
+		(*uint16Alias)(nil),
+		(*uint32)(nil),
+		(*uint32Alias)(nil),
+		(*uintptr)(nil),
+		(*uintptrAlias)(nil),
+	)
+}
+
+type boolAlias bool
+type byteAlias byte
+type float32Alias float32
+type float64Alias float64
+type int8Alias int8
+type int16Alias int16
+type int32Alias int32
+type int64Alias int64
+type stringAlias string
+type uint8Alias uint8
+type uint16Alias uint16
+type uint32Alias uint32
+type uintptrAlias uintptr
