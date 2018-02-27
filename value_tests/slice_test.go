9472474ffd5d9922c63674261e7cbf1a1555fc5e
Tao Wen <taowen@gmail.com>
2018-02-27 12:04:11 +0800

    test []byte behavior

--- value_tests/slice_test.go
+++ value_tests/slice_test.go
@@ -6,6 +6,7 @@ func init() {
 		[]interface{}{"hello"},
 		nilSlice,
 		&nilSlice,
+		selectedMarshalCase{[]byte{1,2,3}},
 	)
 	unmarshalCases = append(unmarshalCases, unmarshalCase{
 		ptr:   (*[]string)(nil),
@@ -13,5 +14,12 @@ func init() {
 	}, unmarshalCase{
 		ptr:   (*[]string)(nil),
 		input: "[]",
+	}, unmarshalCase{
+		ptr:   (*[]byte)(nil),
+		input: "[1,2,3]",
+	}, unmarshalCase{
+		ptr:   (*[]byte)(nil),
+		input: `"aGVsbG8="`,
+		selected: true,
 	})
 }
