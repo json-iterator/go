d58dad7e8135d745646ec2c0db0bd555921b29de
Felipe Pieretti Umpierre <felipe.pieretti@solarisbank.de>
2018-08-03 15:26:39 +0200

    Allow private fields to have a custom json tag

--- extra/privat_fields.go
+++ extra/privat_fields.go
@@ -1,9 +1,10 @@
 package extra
 
 import (
-	"github.com/json-iterator/go"
 	"strings"
 	"unicode"
+
+	"github.com/json-iterator/go"
 )
 
 // SupportPrivateFields include private fields when encoding/decoding
@@ -45,10 +46,12 @@ func calcFieldNames(originalFieldName string, tagProvidedFieldName string, whole
 	} else {
 		fieldNames = []string{tagProvidedFieldName}
 	}
-	// private?
-	isNotExported := unicode.IsLower(rune(originalFieldName[0]))
-	if isNotExported {
+
+	// not private?
+	isExported := unicode.IsUpper(rune(originalFieldName[0]))
+	if isExported {
 		fieldNames = []string{}
 	}
+
 	return fieldNames
 }
--- extra/private_fields_test.go
+++ extra/private_fields_test.go
@@ -1,9 +1,10 @@
 package extra
 
 import (
+	"testing"
+
 	"github.com/json-iterator/go"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func Test_private_fields(t *testing.T) {
@@ -14,5 +15,16 @@ func Test_private_fields(t *testing.T) {
 	should := require.New(t)
 	obj := TestObject{}
 	should.Nil(jsoniter.UnmarshalFromString(`{"field1":"Hello"}`, &obj))
+	should.Equal("Hello", obj.field1)
+}
+
+func Test_private_fields_with_tag(t *testing.T) {
+	type TestObject struct {
+		field1 string `json:"field_1"`
+	}
+	SupportPrivateFields()
+	should := require.New(t)
+	obj := TestObject{}
+	should.Nil(jsoniter.UnmarshalFromString(`{"field_1":"Hello"}`, &obj))
 	should.Equal("Hello", obj.field1)
 }
