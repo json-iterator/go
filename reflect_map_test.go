8f6f519aafea44e4517820bca1597230639aa2f7
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-01-15 16:58:54 +0300

    Update reflect_map_test.go
    Change package to jsoniter_test.
    Add test of MarshalIndent() function

--- reflect_map_test.go
+++ reflect_map_test.go
@@ -1,14 +1,41 @@
-package jsoniter
+package jsoniter_test
 
 import (
 	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
-func TestEncoderIndentionStep(t *testing.T) {
+func TestMapIndention(t *testing.T) {
 	should := require.New(t)
+
+	type object struct {
+		A string `json:"a"`
+		B string `json:"b"`
+	}
+
+	testStruct := struct {
+		M      map[int]object    `json:"first-map"`
+		M2     map[string]object `json:"second-map"`
+		S      object            `json:"custom-struct"`
+		Number int               `json:"number"`
+	}{
+		M: map[int]object{
+			0: object{"some", "test"},
+			1: object{"some", "test"},
+			2: object{"some", "test"},
+		},
+		M2: map[string]object{
+			"first":  object{"321", "123"},
+			"second": object{"321", "123"},
+			"third":  object{"321", "123"},
+		},
+		S:      object{"some", "struct"},
+		Number: 42,
+	}
 
 	res := `{
     "first-map": {
@@ -44,45 +71,27 @@ func TestEncoderIndentionStep(t *testing.T) {
         "b": "struct"
     },
     "number": 42
-}
-`
+}`
 
-	json := Config{
+	json := jsoniter.Config{
 		EscapeHTML:  true,
 		SortMapKeys: true,
 	}.Froze()
 
-	type Struct struct {
-		A string `json:"a"`
-		B string `json:"b"`
-	}
+	// Test MarshalIndent
+	data, err := json.MarshalIndent(testStruct, "", "    ")
+	should.Nil(err)
+	should.Equal(res, string(data))
 
-	ForTest := struct {
-		M      map[int]Struct    `json:"first-map"`
-		M2     map[string]Struct `json:"second-map"`
-		S      Struct            `json:"custom-struct"`
-		Number int               `json:"number"`
-	}{
-		M: map[int]Struct{
-			0: Struct{"some", "test"},
-			1: Struct{"some", "test"},
-			2: Struct{"some", "test"},
-		},
-		M2: map[string]Struct{
-			"first":  Struct{"321", "123"},
-			"second": Struct{"321", "123"},
-			"third":  Struct{"321", "123"},
-		},
-		S:      Struct{"some", "struct"},
-		Number: 42,
-	}
-
+	// Test Encoder. Have to add '\n' into res because Encode() adds additional '\n'
+	res += "\n"
 	buff := bytes.NewBuffer(nil)
 
 	enc := json.NewEncoder(buff)
 	enc.SetIndent("", "    ")
-	enc.Encode(ForTest)
+	enc.Encode(testStruct)
 
 	should.Equal(res, buff.String())
-	// io.Copy(os.Stdout, buff)
+
+	// io.Copy(os.Stdout, buff) // for debug
 }
