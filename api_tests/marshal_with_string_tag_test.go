d14e9e3c18fd9412d811fb54b8ee5e5660d863e2
张朋 <zhangpeng.backend@bytedance.com>
2019-07-14 11:35:58 +0800

    add pointer elem test case

--- api_tests/marshal_with_string_tag_test.go
+++ api_tests/marshal_with_string_tag_test.go
@@ -1,37 +1,46 @@
 package test
 
 import (
-	"fmt"
+	"testing"
+
 	jsoniter "github.com/json-iterator/go"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 type Obj struct {
-	Bar []int64 `json:",,string"`
+	Bar []int64  `json:",,string"`
+	Foo []*int `json:",string"`
 }
 
 func TestMarshal(t *testing.T) {
 	should := require.New(t)
+	a, b, c := 1, 2, 3
+	foo := []*int{&a, &b, &c, nil}
 	obj := Obj{
 		Bar: []int64{
 			1, 2, 3, 4,
 		},
+		Foo: foo,
 	}
 
-	str, err := jsoniter.Marshal(&obj)
+	out, err := jsoniter.Marshal(&obj)
 	should.Nil(err)
-	should.NotEmpty(str)
-	fmt.Println(string(str))
+	should.NotEmpty(out)
+	should.JSONEq(`{"Bar":["1","2","3","4"], "Foo": ["1", "2", "3", "null"]}`, string(out))
 }
 
 func TestUnmarshal(t *testing.T) {
 	should := require.New(t)
-	str := `{"Bar":["1","2","3","4", "122313213"]}`
+	str := `{"Bar":["1","2","3","4", "122313213"], "Foo": ["1", "2", "3", "null"]}`
 	obj := Obj{}
 	err := jsoniter.Unmarshal([]byte(str), &obj)
 	should.NotNil(obj)
 	should.Nil(err)
-	fmt.Println(obj)
+	should.NotNil(obj.Bar)
+	should.NotNil(obj.Foo)
+	should.Equal(obj.Bar[0], int64(1))
+	should.Equal(*obj.Foo[0], 1)
+	should.Equal(*obj.Foo[1], 2)
+	should.Equal(*obj.Foo[2], 3)
+	should.Nil(obj.Foo[3])
 }
-
